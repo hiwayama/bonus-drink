f70a208a99cb4c0eaf1a2047d3c858872eb10fe5
hiwayama <iwayma1880@gmail.com>
2019-04-27 02:21:42 +0900

    実装完了

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,29 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total_count = 0
+    drink_num = amount
+    empty_drink_num = 0
+    while drink_num > 0
+      total_count += drink_num
+      result = exchange(drink_num)
+      drink_num = result[:drink_num]
+      empty_drink_num += result[:empty_drink_num]
+      # 空瓶が溜まったら追加で交換可能
+      if empty_drink_num >= 3
+        drink_num += empty_drink_num / 3
+        empty_drink_num %= 3
+        redo
+      end
+    end
+    total_count
+  end
+
+  private
+
+  def self.exchange amount
+    {
+      drink_num: amount / 3,
+      empty_drink_num: amount % 3,
+    }
   end
 end
